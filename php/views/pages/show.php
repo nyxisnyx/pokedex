9a6d5a1b9ef197031b81bebb429de5c68dc063c6
Caroline <deconinck.caro@gmail.com>
2024-06-24 10:48:18 +0200

    trying to fix path erros

--- php/views/pages/show.php
+++ php/views/pages/show.php
@@ -1,6 +1,6 @@
 <?php
 $title = $_GET['name'];
-require_once __DIR__ . '../../partials/header.php';
+require_once '../partials/head.php';
 ?>
 
 <main>
@@ -11,5 +11,5 @@ require_once __DIR__ . '../../partials/header.php';
 </main>
 
 <?php
-require_once __DIR__ . '../../partials/footer.php';
+require_once '../partials/end.php';
 ?>
--- php/views/partials/head.php
+++ php/views/partials/head.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo htmlspecialchars($title); ?> - Pokedex</title>
-    <link rel="stylesheet" href="./../../assets/style.css">
     <link href="../../../assets/css/styles.css" type="text/css" rel="stylesheet">
 </head>
 
