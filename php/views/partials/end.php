54e133bfa2cf6f5018fb789a9327b7fde6697b88
JulieWustefeld <juliewustefeld@gmail.com>
2024-06-26 10:06:14 +0200

    Design footer

--- php/views/partials/end.php
+++ php/views/partials/end.php
@@ -1,3 +1,11 @@
+<footer>
+    <div class="githubLinks">
+        <a href="https://github.com/MaryamAkraiche"><img src="../../../assets/images/github.svg" alt="Github's icon"></a>
+        <a href="https://github.com/Caroline1123"><img src="../../../assets/images/github.svg" alt="Github's icon"></a>
+        <a href="https://github.com/nyxisnyx"><img src="../../../assets/images/github.svg" alt="Github's icon"></a>
+    </div>
+    <p>Source material belongs to Nintendo, Game Freak, and Creature. Gotta catch 'em all!</p>
+</footer>
 </body>
 
 </html>
