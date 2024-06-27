c6722bf9b5463747c7d139ec20e4fca56dadced3
JulieWustefeld <juliewustefeld@gmail.com>
2024-06-27 15:23:07 +0200

    Preparing show.php for flex design

--- php/views/pages/show.php
+++ php/views/pages/show.php
@@ -116,104 +116,109 @@ if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     <main>
         <?php if ($pokemon): ?>
             <div class="pokemon-details">
-                <h1><?php echo htmlspecialchars($pokemon['name']) ?? 'Pokemon Details'; ?></h1>
-                <img class="poke-image" src="../../../assets/images/pokemon/<?php echo htmlspecialchars($pokemon['imageBig']); ?>" alt="<?php echo htmlspecialchars($pokemon['name']); ?> sprite">
-                <p><?php echo htmlspecialchars($pokemon["ID"]); ?></p>
-                <p><?php echo htmlspecialchars($pokemon['type1']); ?><?php if (!empty($pokemon['type2'])) echo '   ' . htmlspecialchars($pokemon['type2']); ?></p>
-                <p><strong>Description:</strong> <?php echo htmlspecialchars($pokemon['description']); ?></p>
+                <div class="detailsleft">
+                    <h1><?php echo htmlspecialchars($pokemon['name']) ?? 'Pokemon Details'; ?></h1>
+                    
+                    <p><?php echo htmlspecialchars($pokemon['type1']); ?><?php if (!empty($pokemon['type2'])) echo '   ' . htmlspecialchars($pokemon['type2']); ?></p>
+                    <p><strong>Description:</strong> <?php echo htmlspecialchars($pokemon['description']); ?></p>
 
-                <div class="stats">
-                    <div class="stat-bar">
-                        <span>HP</span>
-                        <div class="bar">
-                            <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['HP']); ?>%;"></div>
+                    <div class="stats">
+                        <div class="stat-bar">
+                            <span>HP</span>
+                            <div class="bar">
+                                <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['HP']); ?>%;"></div>
+                            </div>
+                        </div>
+                        <div class="stat-bar">
+                            <span>Attack</span>
+                            <div class="bar">
+                                <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Attack']); ?>%;"></div>
+                            </div>
+                        </div>
+                        <div class="stat-bar">
+                            <span>Defense</span>
+                            <div class="bar">
+                                <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Defense']); ?>%;"></div>
+                            </div>
+                        </div>
+                        <div class="stat-bar">
+                            <span>Specific Attack</span>
+                            <div class="bar">
+                                <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Sp. Attack']); ?>%;"></div>
+                            </div>
+                        </div>
+                        <div class="stat-bar">
+                            <span>Specific Defense</span>
+                            <div class="bar">
+                                <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Sp. Defense']); ?>%;"></div>
+                            </div>
+                        </div>
+                        <div class="stat-bar">
+                            <span>Speed</span>
+                            <div class="bar">
+                                <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Speed']); ?>%;"></div>
+                            </div>
                         </div>
                     </div>
-                    <div class="stat-bar">
-                        <span>Attack</span>
-                        <div class="bar">
-                            <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Attack']); ?>%;"></div>
-                        </div>
-                    </div>
-                    <div class="stat-bar">
-                        <span>Defense</span>
-                        <div class="bar">
-                            <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Defense']); ?>%;"></div>
-                        </div>
-                    </div>
-                    <div class="stat-bar">
-                        <span>Specific Attack</span>
-                        <div class="bar">
-                            <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Sp. Attack']); ?>%;"></div>
-                        </div>
-                    </div>
-                    <div class="stat-bar">
-                        <span>Specific Defense</span>
-                        <div class="bar">
-                            <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Sp. Defense']); ?>%;"></div>
-                        </div>
-                    </div>
-                    <div class="stat-bar">
-                        <span>Speed</span>
-                        <div class="bar">
-                            <div class="bar-fill" style="width: <?php echo htmlspecialchars($pokemon['Speed']); ?>%;"></div>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="evolution">
+                    <div class="evolution">
                 
-                    <?php if ($prevEvolution): ?>
-                        <div class="evolution-card">
-                            <a href="show.php?id=<?php echo htmlspecialchars($prevEvolution['ID']); ?>">
-                                <img src="<?php echo htmlspecialchars($prevEvolution['imageThumbnail']); ?>" alt="<?php echo htmlspecialchars($prevEvolution['name']); ?>">
-                                <p><?php echo htmlspecialchars($prevEvolution['name']); ?></p>
-                            </a>
-                        </div>
-                        <?php // Display all previous evolutions of the previous evolution ?>
-                    <?php if (!empty($allPrevEvolutions)): ?>
-                        <?php foreach ($allPrevEvolutions as $prevEvolution): ?>
+                        <?php if ($prevEvolution): ?>
                             <div class="evolution-card">
                                 <a href="show.php?id=<?php echo htmlspecialchars($prevEvolution['ID']); ?>">
                                     <img src="<?php echo htmlspecialchars($prevEvolution['imageThumbnail']); ?>" alt="<?php echo htmlspecialchars($prevEvolution['name']); ?>">
                                     <p><?php echo htmlspecialchars($prevEvolution['name']); ?></p>
                                 </a>
                             </div>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
-                    <?php else: ?>
-                        <div class="evolution-card"></div>
-                    <?php endif; ?>
+                            <?php // Display all previous evolutions of the previous evolution ?>
+                        <?php if (!empty($allPrevEvolutions)): ?>
+                            <?php foreach ($allPrevEvolutions as $prevEvolution): ?>
+                                <div class="evolution-card">
+                                    <a href="show.php?id=<?php echo htmlspecialchars($prevEvolution['ID']); ?>">
+                                        <img src="<?php echo htmlspecialchars($prevEvolution['imageThumbnail']); ?>" alt="<?php echo htmlspecialchars($prevEvolution['name']); ?>">
+                                        <p><?php echo htmlspecialchars($prevEvolution['name']); ?></p>
+                                    </a>
+                                </div>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                        <?php else: ?>
+                            <div class="evolution-card"></div>
+                        <?php endif; ?>
                     
-                    <div class="evolution-card">
-                        <img src="<?php echo htmlspecialchars($pokemon['imageThumbnail']); ?>" alt="<?php echo htmlspecialchars($pokemon['name']); ?>">
-                        <p><?php echo htmlspecialchars($pokemon['name']); ?></p>
-                    </div>
-                    
-                    <?php if ($nextEvolution): ?>
                         <div class="evolution-card">
-                            <a href="show.php?id=<?php echo htmlspecialchars($nextEvolution['ID']); ?>">
-                                <img src="<?php echo htmlspecialchars($nextEvolution['imageThumbnail']); ?>" alt="<?php echo htmlspecialchars($nextEvolution['name']); ?>">
-                                <p><?php echo htmlspecialchars($nextEvolution['name']); ?></p>
-                            </a>
+                            <img src="<?php echo htmlspecialchars($pokemon['imageThumbnail']); ?>" alt="<?php echo htmlspecialchars($pokemon['name']); ?>">
+                            <p><?php echo htmlspecialchars($pokemon['name']); ?></p>
                         </div>
-                    <?php else: ?>
-                        <div class="evolution-card"></div>
-                    <?php endif; ?>
                     
-                    
-                    
-                    <?php // Display all next evolutions of the next evolution ?>
-                    <?php if (!empty($allNextEvolutions)): ?>
-                        <?php foreach ($allNextEvolutions as $nextEvolution): ?>
+                        <?php if ($nextEvolution): ?>
                             <div class="evolution-card">
                                 <a href="show.php?id=<?php echo htmlspecialchars($nextEvolution['ID']); ?>">
                                     <img src="<?php echo htmlspecialchars($nextEvolution['imageThumbnail']); ?>" alt="<?php echo htmlspecialchars($nextEvolution['name']); ?>">
                                     <p><?php echo htmlspecialchars($nextEvolution['name']); ?></p>
                                 </a>
                             </div>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
+                        <?php else: ?>
+                            <div class="evolution-card"></div>
+                        <?php endif; ?>
+                    
+                    
+                    
+                        <?php // Display all next evolutions of the next evolution ?>
+                        <?php if (!empty($allNextEvolutions)): ?>
+                            <?php foreach ($allNextEvolutions as $nextEvolution): ?>
+                                <div class="evolution-card">
+                                    <a href="show.php?id=<?php echo htmlspecialchars($nextEvolution['ID']); ?>">
+                                        <img src="<?php echo htmlspecialchars($nextEvolution['imageThumbnail']); ?>" alt="<?php echo htmlspecialchars($nextEvolution['name']); ?>">
+                                        <p><?php echo htmlspecialchars($nextEvolution['name']); ?></p>
+                                    </a>
+                                </div>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+                </div>
+                <div class="detailsright">
+                    <img class="poke-image" src="../../../assets/images/pokemon/<?php echo htmlspecialchars($pokemon['imageBig']); ?>" alt="<?php echo htmlspecialchars($pokemon['name']); ?> sprite">
+                    <p><?php echo htmlspecialchars($pokemon["ID"]); ?></p>
                 </div>
             </div>
         <?php else: ?>
